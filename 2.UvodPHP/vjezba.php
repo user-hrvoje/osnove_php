2ff896551ecbdd4b5daf529eeb29782c34232751
Hrvoje Jug <you@example.com>
2024-07-27 21:45:42 +0200

    promjena

--- 2.UvodPHP/vjezba.php
+++ 2.UvodPHP/vjezba.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+$integer = 10;
+$float = 3.5;
+$string = "Algebra";
+$bool = true;
