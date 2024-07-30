0a7b88580592f32ee58efef3136cec631948b3df
Hrvoje Jug <you@example.com>
2024-07-30 21:31:55 +0200

    2

--- 3.Operatori/inicijalizacija.php
+++ 3.Operatori/inicijalizacija.php
@@ -3,7 +3,7 @@
 $empty1 = [];
 $empty2 = array();
 
-// Inicijalizacija niza (lista) s podacima
+// Inicijalizacija niza (lista) s podacima -> Indexed array
 
 $names1 = ['Ivo', 'Pavo', 'Marija', 'Ana'];
 $names2 = array('Ivo', 'Pavo', 'Marija', 'Ana');
@@ -11,7 +11,18 @@ $names2 = array('Ivo', 'Pavo', 'Marija', 'Ana');
 echo '<pre>';
 
 print_r($names1);
-
 echo '</pre>';
-
 echo $names2[1];
+
+// Inicijalizacija niza (mapa) s podacima -> Associative array
+
+$status = ["name" => "Banana",
+           "cijena" => "12 €",
+           "klasa" => "Prva"
+        ];
+echo '<pre>';
+print_r($status);
+echo '</pre>';
+echo $status['name'];
+
+
