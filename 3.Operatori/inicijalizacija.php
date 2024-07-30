ad2620608f805979e096e2667af0c9f9b049984d
Hrvoje Jug <you@example.com>
2024-07-30 20:12:19 +0200

    first commit

---
+++ 3.Operatori/inicijalizacija.php
@@ -0,0 +1,17 @@
+<?php
+// Inicijalizacija praznog niza
+$empty1 = [];
+$empty2 = array();
+
+// Inicijalizacija niza (lista) s podacima
+
+$names1 = ['Ivo', 'Pavo', 'Marija', 'Ana'];
+$names2 = array('Ivo', 'Pavo', 'Marija', 'Ana');
+
+echo '<pre>';
+
+print_r($names1);
+
+echo '</pre>';
+
+echo $names2[1];
